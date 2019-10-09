cac45de3e93f0e2eb0331bdb4ca0b623f881ad35
Miftah-Maritah <muhammadmiftah998@gmail.com>
2019-10-09 21:58:19 +0700

    Add ListTile Di Drawer untuk memilih menu yang ada di apps

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -14,24 +14,33 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.cyan,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.search),
-            onPressed: ()=>debugPrint("Search")),
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: ()=>debugPrint("Add"))
+              icon: Icon(Icons.search), onPressed: () => debugPrint("Search")),
+          IconButton(icon: Icon(Icons.add), onPressed: () => debugPrint("Add"))
         ],
-
       ),
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
             UserAccountsDrawerHeader(
               accountName: Text("Muhammad Miftah"),
-              accountEmail: Text("muhammad.miftah.ft18@mail.umy.ac.id"),)
+              accountEmail: Text("muhammad.miftah.ft18@mail.umy.ac.id"),
+              decoration: BoxDecoration(color: Colors.cyan),
+            ),
+            ListTile(
+              title: Text("First Page"),
+              leading: Icon(Icons.home, color: Colors.cyan),
+            ),
+            ListTile(
+              title: Text("First Page"),
+              leading: Icon(Icons.home, color: Colors.cyan),
+            ),
+            ListTile(
+              title: Text("First Page"),
+              leading: Icon(Icons.home, color: Colors.cyan),
+            )
           ],
         ),
       ),
-      
     );
   }
 }
